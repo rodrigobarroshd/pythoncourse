b8d9655f2ff47fac521290c9e92713bebde4b687
Rodrigo Barros <brodrigo87@gmail.com>
2022-05-03 11:42:38 -0300

    print

---
+++ 1.logic/1.print.py
@@ -0,0 +1,11 @@
+print (123456)
+
+
+# print('Rodrigo', 'Jotelly', 'Jhon', 'Arthur')
+# print('Rodrigo', 'Jotelly', 'Jhon', 'Arthur', sep=', ', end='.' ) # sep = Separate, End = End
+
+
+# 824.176.070-18
+
+print('824', '176', '070', sep='.', end='-')
+print('18', sep='-')
