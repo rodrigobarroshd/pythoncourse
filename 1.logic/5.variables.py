e76d7a92fb892047b2cb1d3c435e9ced82f0b22b
Rodrigo Barros <brodrigo87@gmail.com>
2022-05-04 10:38:43 -0300

    variables

---
+++ 1.logic/5.variables.py
@@ -0,0 +1,14 @@
+# Variables
+
+
+name = ('Rodrigo')
+age = 20 # int
+height = 1.73 # float
+weight = 62 # int
+is_old = age > 18 # bool
+data_1 = True # bool
+data_atual = 2022 # int
+
+
+imc = weight /(height * height)
+print(name, 'is', age, 'years old', 'and your IMC is',"{:.2f}".format(imc))
