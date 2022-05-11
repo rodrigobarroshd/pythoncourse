8a2b96ab3187f72675bb586ce6efae3093df166b
Rodrigo Barros <brodrigo87@gmail.com>
2022-05-11 17:01:25 -0300

    if elif else

--- 1.logic/11.if_elif_else.py
+++ 1.logic/11.if_elif_else.py
@@ -6,17 +6,46 @@
 # (True    and    False) = False
 # comparacao1 and comparacao2
 
-name = input('Whats your name? ')
-age = input('Whats your age? ')
+# (True or True)
+# comp1 or comp2
 
-age = int(age)
+# name = input('Whats your name? ')
+# age = input('Whats your age? ')
 
-# Limite para pegarr empréstimo
+# age = int(age)
 
-age_more = 20 # So much young
-age_less = 30 # So much old
+# # Limite para pegar empréstimo
 
-if age >= age_less and age <= age_more:
-    print(f'{name} pode pegar o empréstimo.')
+# age_more = 20 # So much young
+# age_less = 30 # So much old
+
+# if age >= age_less and age <= age_more:
+#     print(f'{name} pode pegar o empréstimo.')
+# else:
+#     print(f'{name} não pode pegar o empréstimo. ')
+
+
+#################
+
+# a = 2
+# b = 3
+
+# if b > a:
+#     print('B é maior do que A')
+# else:
+#     print('A é maior do que B')
+
+# if not b > a:
+#     print('B é maior do que A')
+# else:
+#     print('A é maior do que B')
+
+
+###############
+
+name = 'Rodrigo'
+
+if 'u' in name:
+    print('Existe a letra U no seu nome. ')
 else:
-    print(f'{name} não pdoe pegar o empréstimo. ')
+    print('Não existe a letra U no seu nome')
