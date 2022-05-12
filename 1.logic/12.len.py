d87321d4f61fda5e1aedd148accec896e1c67632
Rodrigo Barros <brodrigo87@gmail.com>
2022-05-12 15:36:31 -0300

    function ( len )

---
+++ 1.logic/12.len.py
@@ -0,0 +1,19 @@
+################################################################################################
+
+# Len - Quantidade de caracteres
+
+# name = "Rodrigo Barros"
+# qtd_caracteres = len(name)
+
+# if qtd_caracteres < 4:
+#     print('Voce precisa digitar pelo menos 6 caracteres...')
+# else:
+#     print('Voce foi cadastrado no sistema...')
+    
+
+################################################################################################
+
+string1 = 'Rodrigo'
+string2 = 'Python'
+
+print(f'o nome ({string1}) possui {len(string1)} caracteres e o nome ({string2}) possui {len(string2)} caracteres')
