dc9296daef5b84d924d86f1953f2b74ec575dbdc
Rodrigo Barros <brodrigo87@gmail.com>
2022-05-12 15:52:16 -0300

    built in

---
+++ 1.logic/13.built_in.py
@@ -0,0 +1,28 @@
+# Built in Types
+
+# Boolean Operations - and, or, not
+
+# x or y - if x is false, then y, else x
+# x and y - if x is false, then x, else y
+# not x - if x is false, then (True), else (False)
+
+
+##################################################################################
+
+# Comparisons
+
+# < strictly less than
+
+#<= less than or equal
+
+#> strictly greater than
+
+#>= greater than or equal
+
+#== equal
+
+#!= not equal
+
+#is object identity
+
+#is not negated object identity
