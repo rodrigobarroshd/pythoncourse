e14342ce2c6b80504f2a3b02b4a8bd76ec6decba
Rodrigo Barros <brodrigo87@gmail.com>
2022-05-26 15:16:24 -0300

    for in

--- 1.logic/21.for_in.py
+++ 1.logic/21.for_in.py
@@ -3,4 +3,25 @@
 # Iterando strings com FOR
 # Função range(start=0, stop, step=1)
 
+# texto = 'Python'
+
+
+# for n in range(0, 20, 3):  # Vai do 0 ao 20 pulando de 3 em 3...
+#     print(n)
+    
+
+##################################################
+
 texto = 'Python'
+nova_string = ''
+
+for letra in texto:
+    if letra == 't':
+        nova_string = nova_string + letra.upper()
+    elif letra == 'h':
+        nova_string += letra.upper()
+        break
+    else:
+        nova_string += letra
+        
+print(nova_string)
