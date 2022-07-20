d42a1621d903d92a32f243d92e83e675c7e797b2
Rodrigo Barros <brodrigo87@gmail.com>
2022-07-20 16:49:57 -0300

    2

---
+++ 3.intermediary/2.funcoes_def2.py
@@ -0,0 +1,16 @@
+# Funções (def) em Python - return 
+
+
+
+def divisao(n1, n2):
+    if n2 > 0:
+        return
+
+    return n1 / n2
+
+divide = divisao(8,2)
+
+if divide:
+    print(divide)
+else:
+    print('Conta inválida')
