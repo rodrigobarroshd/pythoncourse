ac3deacc5df3d4a3ac77a19f8368bcae2bc778c3
Rodrigo Barros <brodrigo87@gmail.com>
2022-07-20 17:33:32 -0300

    3.4

--- 3.intermediary/3.funcoes_ex.py
+++ 3.intermediary/3.funcoes_ex.py
@@ -15,41 +15,51 @@
 
 # 2 - Crie uma função que recebe 3 números como parâmetros e exiba a soma entre eles.
 
-def soma(n1, n2, n3):
-    return n1 + n2 + n3
 
-somar = soma(8, 2, 14)
+# def soma(n1, n2, n3):
+#     return n1 + n2 + n3
 
-if somar:
-    print(somar)
+# somar = soma(8, 2, 14)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# if somar:
+#     print(somar)
 
 
 
 # 3 - Crie uma função que receba 2 números. O primeiro é um valor e o segundo um percentual (ex. 10%)
 # Retorne (return) o valor do primeiro número somado do aumento do percentual do mesmo.
 
+
+# def aumento_percentual(valor, percentual):
+#     return valor + (valor * percentual / 100)
+
+# ap = aumento_percentual(50, 10)
+# print(ap)
+# ap = aumento_percentual(100, 10)
+# print(ap)
+# ap = aumento_percentual(10, 10)
+# print(ap)
+# ap = aumento_percentual(15, 100)
+# print(ap)
+
+
+
+
+
 # 4 - Fizz Buzz - Se o parâmetro da função for divisível por 2, retorne fizz, se o parâmetro da função
 # for divisível por 5 e por 3, retorne FizzBuzz, caso contrário, retorne o número enviado
+
+
+def fb(n):
+    if n % 3 == 0 and n % 5 == 0:
+        return 'fizzbuzz'
+    if n % 5 == 0:
+        return 'buzz'
+    if n % 3 == 0:
+        return 'fizz'
+    else:
+        return n
+
+print(fb(15))
+print(fb(7))
+print(fb(10))
