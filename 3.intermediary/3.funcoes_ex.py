59e40fd7060ce58c79d63a6c513e82f5aa692c8e
Rodrigo Barros <50782890+rodrigobarroshd@users.noreply.github.com>
2022-07-21 18:08:10 -0300

    Update 3.funcoes_ex.py

--- 3.intermediary/3.funcoes_ex.py
+++ 3.intermediary/3.funcoes_ex.py
@@ -46,7 +46,7 @@ print(ap)
 
 
 
-# 4 - Fizz Buzz - Se o parâmetro da função for divisível por 2, retorne fizz, se o parâmetro da função
+# 4 - Fizz Buzz ! - Se o parâmetro da função for divisível por 2, retorne fizz, se o parâmetro da função
 # for divisível por 5 e por 3, retorne FizzBuzz, caso contrário, retorne o número enviado
 
 
